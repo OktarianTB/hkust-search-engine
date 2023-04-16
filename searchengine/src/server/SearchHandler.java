cf44be178935178e3e6d7e93372e9b88e16343d4
OktarianTB <oktotb@gmail.com>
2023-04-16 20:16:33 +0800

    handle CORS

--- searchengine/src/server/SearchHandler.java
+++ searchengine/src/server/SearchHandler.java
@@ -10,6 +10,7 @@ import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -17,12 +18,14 @@ import engine.Engine;
 import utilities.Result;
 
 class SearchHandler implements HttpHandler {
+    Gson gson;
+
     SearchHandler() throws IOException {
+        gson = new Gson();
     }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        Gson gson = new Gson();
         Engine engine = new Engine();
 
         String requestMethod = exchange.getRequestMethod();
@@ -44,13 +47,19 @@ class SearchHandler implements HttpHandler {
             List<Result> results = engine.search(query);
 
             // convert the results to a list of JsonObjects
-            List<JsonObject> resultsJson = results.stream().map(Result::toJson).collect(java.util.stream.Collectors.toList());
+            List<JsonObject> resultsJson = results.stream().map(Result::toJson)
+                    .collect(java.util.stream.Collectors.toList());
 
             // create json response object
             JsonObject responseBodyJson = new JsonObject();
             responseBodyJson.add("results", gson.toJsonTree(resultsJson));
-            
+
             // set the response headers
+            Headers headers = exchange.getResponseHeaders();
+            headers.add("Access-Control-Allow-Origin", "http://localhost:3000");
+            headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+            headers.add("Access-Control-Allow-Headers", "Content-Type");
+
             exchange.getResponseHeaders().set("Content-Type", "application/json");
             exchange.sendResponseHeaders(200, 0);
 
@@ -60,6 +69,14 @@ class SearchHandler implements HttpHandler {
             gson.toJson(responseBodyJson, writer);
             writer.flush();
             writer.close();
+        } else if (requestMethod.equalsIgnoreCase("OPTIONS")) {
+            Headers headers = exchange.getResponseHeaders();
+            headers.add("Access-Control-Allow-Origin", "http://localhost:3000");
+            headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+            headers.add("Access-Control-Allow-Headers", "Content-Type");
+
+            exchange.sendResponseHeaders(200, -1);
+            return;
         } else {
             exchange.sendResponseHeaders(405, -1);
         }
